0a029b2dbc6b2d16aecd54c5fba22b9dd11f7180
soorokim <rlatnfh99@gmail.com>
2022-04-29 00:09:25 +0900

    init

---
+++ src/lib/Memo.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let memoList:string[] = []
+  $: count = memoList.length
+  let value: string = ''
+  $: disabled = value.length === 0
+
+  const increment = () => {
+    memoList = [...memoList, value]
+    value = ''
+  }
+</script>
+
+<input type='text' bind:value />
+<button on:click={increment} disabled={disabled}>
+  메모추가: {count}
+</button>
+{#each memoList as memo}
+<p>{memo}</p>
+{/each}
